8d43b661eeefa9fd4afa22c40ecb3d79c0e258e5
Stefan SL <ssl@clickpress.de>
2017-03-02 14:27:29 +0100

    Add mac app

---
+++ release-builds/SVG Shrinker-darwin-x64/SVG Shrinker.app/Contents/Resources/app/README.md
@@ -0,0 +1,27 @@
+# SVG Shrinker
+
+SVGO-GUI is not maintained anymore. So I decided to rebuild it with Electron.
+SVG Shrinker is a tool to cleanup SVG graphics. 
+
+## How to use
+Drag you SVG onto the SVG Shrinker window and it will saved in the same folder as reduced SVG.
+The original graphic will be not replaced.
+
+## Installation
+Get the repo
+```shell
+git clone https://lab.clickpress.de/stefan.sl/svg-shrinker.git
+```
+Install dependencies
+```shell
+$ cd svg-shrinker
+$ npm install
+```
+Generate your MacOS package
+```shell
+npm run pack-mac
+```
+Generate your Linux package
+```shell
+npm run pack-linux
+```
---
+++ release-builds/SVG Shrinker-darwin-x64/SVG Shrinker.app/Contents/Resources/app/main.js
@@ -0,0 +1,91 @@
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+const url = require('url');
+const SVGO = require('svgo');
+
+let svgo = new SVGO();
+
+let mainWindow;
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 300,
+        height: 400,
+        frame: true,
+        backgroundColor: '#F7F7F7',
+        icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+// Main logic
+ipcMain.on('shrinkSvg', (event, svgName, svgPath) => {
+
+    fs.readFile(svgPath, 'utf8', function (err, data) {
+
+        if (err) {
+            throw err;
+        }
+
+        if (!checkFileType(svgName)) {
+            dialog.showMessageBox({
+                'type': 'error',
+                'message': 'Only SVG allowed'
+            });
+        } else {
+            let newFile = generateNewPath(svgPath);
+
+            svgo.optimize(data, function (result) {
+                fs.writeFile(newFile, result.data, '', () => {
+
+                });
+
+                event.sender.send('isShrinked', newFile);
+            });
+        }
+    });
+});
+
+
+const checkFileType = fileName => {
+    return (fileName.split('.').pop() === 'svg');
+};
+
+
+const generateNewPath = pathName => {
+    let arrPath = pathName.split('.');
+
+    return arrPath[0] + '.min.' + arrPath[1];
+};
