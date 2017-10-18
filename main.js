018b5009f81d9494ea3ccd3ff3282f63511aa7e3
stefan.sl <ssl@clickpress.de>
2017-10-18 22:40:29 +0200

    Add jpg/png feature

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const url = require('url');
 const SVGO = require('svgo');
+var execFile = require('child_process').execFile;
+var jpegtran = require('jpegtran-bin');
 
 let svgo = new SVGO();
 
@@ -51,36 +53,61 @@ app.on('activate', () => {
 });
 
 // Main logic
-ipcMain.on('shrinkSvg', (event, svgName, svgPath) => {
+ipcMain.on('shrinkSvg', (event, fileName, filePath) => {
 
-    fs.readFile(svgPath, 'utf8', function (err, data) {
+    fs.readFile(filePath, 'utf8', function (err, data) {
 
         if (err) {
             throw err;
         }
 
-        if (!checkFileType(svgName)) {
-            dialog.showMessageBox({
-                'type': 'error',
-                'message': 'Only SVG allowed'
-            });
-        } else {
-            let newFile = generateNewPath(svgPath);
-
-            svgo.optimize(data, function (result) {
-                fs.writeFile(newFile, result.data, '', () => {
-
+        let newFile = generateNewPath(filePath);
+        
+        switch (checkFileType(fileName)) {
+            case 'svg':
+                console.log('SVG, du Schwein');
+                svgo.optimize(data, function (result) {
+                    fs.writeFile(newFile, result.data, '', () => {});
+                    event.sender.send('isShrinked', newFile);
                 });
+                break;
+            case 'jpg':
+            case 'jpeg':
+                execFile(jpegtran, ['-outfile', newFile, filePath], (err) => {
+                    console.log('Image minified!');
+                });
+                break;
+            case 'png':
+                console.log(filePath);
+                console.log(newFile);
 
-                event.sender.send('isShrinked', newFile);
-            });
+                console.log(checkFileType(fileName) + ', du Affe!');
+                imagemin([filePath], newFile, {
+                    plugins: [
+                        imageminJpegtran(),
+                        imageminPngquant({quality: '65-80'})
+                    ]
+                }).then(files => {
+                    console.log(files);
+                    //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+                });
+                break;
+            default:
+                console.log('EAT SHIT AND DIE!');
+                dialog.showMessageBox({
+                    'type': 'error',
+                    'message': 'Only SVG, JPG and PNG allowed'
+                });
         }
+
     });
 });
 
 
 const checkFileType = fileName => {
-    return (fileName.split('.').pop() === 'svg');
+    var mime = fileName.split('.').pop();
+
+    return mime;
 };
 
 
